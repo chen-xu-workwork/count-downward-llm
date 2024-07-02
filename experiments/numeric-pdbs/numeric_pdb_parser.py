b448ca19932f6c08e110b3165baad1a67b8586be
Daniel Gnad <daniel.gnad@liu.se>
2024-07-02 11:44:15 +0200

    merge

--- experiments/numeric-pdbs/numeric_pdb_parser.py
+++ experiments/numeric-pdbs/numeric_pdb_parser.py
@@ -1,13 +1,42 @@
 #! /usr/bin/env python
 
 import re
+import sys
 
 from lab.parser import Parser
 
 
 
+def add_initial_h_values(content, props):
+    """
+    Add a mapping from heuristic names to initial h values.
 
-class DecouplingParser(Parser):
+    If exactly one initial heuristic value was reported, add it to the
+    properties under the name "initial_h_value".
+
+    """
+    initial_h_values = {}
+    matches = re.findall(
+        r"Initial heuristic value for (.+): ([-]?\d+|infinity)$", content, flags=re.M
+    )
+    for heuristic, init_h in matches:
+        if init_h == "infinity":
+            init_h = sys.maxsize
+        else:
+            init_h = float(init_h)
+        if heuristic in initial_h_values:
+            props.add_unexplained_error(
+                f"multiple initial h values found for {heuristic}"
+            )
+        initial_h_values[heuristic] = init_h
+
+    props["initial_h_values_float"] = initial_h_values
+
+    if len(initial_h_values) == 1:
+        props["initial_h_value_float"] = list(initial_h_values.values())[0]
+
+
+class NumericPDBParser(Parser):
     def __init__(self):
         Parser.__init__(self)
         self.add_pattern('number_reached_goal_states', 'Reached abstract goal states: (.+)', required=False, type=int)
@@ -17,7 +46,9 @@ class DecouplingParser(Parser):
         self.add_pattern('number_interesting_patterns', 'Found (.+) interesting patterns.', required=False, type=int)
         self.add_pattern('pdb_collection_construction_time', 'PDB collection construction time: (.+)s', required=False, type=float)
         self.add_pattern('pdb_dominance_pruning_time', 'Dominance pruning took (.+)s', required=False, type=float)
-          
+
+        self.add_function(add_initial_h_values)
+
 
 def get_parser():
-    return DecouplingParser()
+    return NumericPDBParser()
